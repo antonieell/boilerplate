54dfa2c2ec96ba28c6a4cd5f3c4cf42f8e0631da
Antoniel Magalhães <antoniel2210@gmail.com>
2021-02-15 12:17:50 -0300

    feat: create react app with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="items-center h-screen bg-blue-400 grid place-items-center">
+      <div>
+        <h1 role="alert">boilerplate</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
